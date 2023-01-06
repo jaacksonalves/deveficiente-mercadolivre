e946a64d493528e34a3d4a4710fd7e8b67b79d05
jaacksonalves <jacksonalves.pro@gmail.com>
2023-01-06 10:50:39 -0300

    initial commit

---
+++ src/main/java/br/com/deveficientemercadolivre/DeveficienteMercadolivreApplication.java
@@ -0,0 +1,13 @@
+package br.com.deveficientemercadolivre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveficienteMercadolivreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeveficienteMercadolivreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/deveficientemercadolivre/DeveficienteMercadolivreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.deveficientemercadolivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveficienteMercadolivreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

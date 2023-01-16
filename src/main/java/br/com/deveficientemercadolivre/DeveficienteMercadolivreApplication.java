d66f247547d1a966379e9103af7bdf2c2b7d54ea
jaacksonalves <jacksonalves.pro@gmail.com>
2023-01-16 20:01:44 -0300

    feat: cadastrando usuario

--- src/main/java/br/com/deveficientemercadolivre/DeveficienteMercadolivreApplication.java
+++ src/main/java/br/com/deveficientemercadolivre/DeveficienteMercadolivreApplication.java
@@ -1,13 +1,25 @@
 package br.com.deveficientemercadolivre;
 
+import br.com.deveficientemercadolivre.usuario.NovoUsuarioRequest;
+import br.com.deveficientemercadolivre.usuario.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DeveficienteMercadolivreApplication {
+public class DeveficienteMercadolivreApplication implements CommandLineRunner {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(DeveficienteMercadolivreApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) {
+        var usuario = new NovoUsuarioRequest("jackson@email.com", "123456").toModel();
+        usuarioRepository.save(usuario);
+    }
 }
